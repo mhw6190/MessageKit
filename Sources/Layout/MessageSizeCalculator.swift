fa227d24ba06b62e3b647af13767237bb5d4f560
mhw6190 <wainmichael@gmail.com>
2019-04-09 16:08:22 -0400

    Updated incoming/outgoing bottom label alignments

--- Sources/Layout/MessageSizeCalculator.swift
+++ Sources/Layout/MessageSizeCalculator.swift
@@ -47,8 +47,8 @@ open class MessageSizeCalculator: CellSizeCalculator {
     public var incomingMessageTopLabelAlignment = LabelAlignment(textAlignment: .left, textInsets: UIEdgeInsets(left: 42))
     public var outgoingMessageTopLabelAlignment = LabelAlignment(textAlignment: .right, textInsets: UIEdgeInsets(right: 42))
     
-    public var incomingMessageBottomLabelAlignment = LabelAlignment(textAlignment: .left, textInsets: UIEdgeInsets(top: 6, left: 41, bottom: 0, right: 0))
-    public var outgoingMessageBottomLabelAlignment = LabelAlignment(textAlignment: .right, textInsets: UIEdgeInsets(top: 6, left: 0, bottom: 0, right: 12))
+      public var incomingMessageBottomLabelAlignment = LabelAlignment(textAlignment: .left, textInsets: UIEdgeInsets(top: 10, left: 41, bottom: 7, right: 0))
+    public var outgoingMessageBottomLabelAlignment = LabelAlignment(textAlignment: .right, textInsets: UIEdgeInsets(top: 10, left: 0, bottom: 7, right: 12))
     
     open override func configure(attributes: UICollectionViewLayoutAttributes) {
         guard let attributes = attributes as? MessagesCollectionViewLayoutAttributes else { return }
